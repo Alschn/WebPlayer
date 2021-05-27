e791c8c1388805c79031d6a0a1671f2881c29108
Alschn <adam.lisichin@gmail.com>
2021-05-28 00:37:15 +0200

    Add more spotify endpoints
    Added multiple empty endpoints, working GetPlaylist, GetUserPlaylists. Added HasSpotifyToken permission. Added some utility functions to be used later (ported from other project)

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,10 +1,26 @@
 from django.urls import path
 
-from .views import GetSpotifyAuthURL, GetSpotifyAccessToken, SpotifyLogin, SpotifyLoginHandler
+from .views import *
 
 urlpatterns = [
+    # auth endpoints
     path('spotify-url', GetSpotifyAuthURL.as_view()),
     path('auth/spotify-token', GetSpotifyAccessToken.as_view()),
     path('auth/spotify-login', SpotifyLoginHandler.as_view()),
-    path('auth/login', SpotifyLogin.as_view())
+    path('auth/login', SpotifyLogin.as_view()),
+    # Spotify endpoints
+    path('spotify/playlists', GetUserPlaylists.as_view()),
+    path('spotify/playlists/<str:id>', GetPlaylist.as_view()),
+    path('spotify/play', PlaySong.as_view()),
+    path('spotify/pause', PauseSong.as_view()),
+    path('spotify/skip', SkipSong.as_view()),
+    path('spotify/volume', SetVolume.as_view()),
+    path('spotify/playback', SetPlaybackMode.as_view()),
+    path('spotify/repeat', SetRepeatMode.as_view()),
+    path('spotify/queue', AddToQueue.as_view()),
+    path('spotify/recommendations', GetRecommendations.as_view()),
+    path('spotify/song', GetCurrentSong.as_view()),
+    path('spotify/user', GetCurrentUser.as_view()),
+    path('spotify/library', GetUserLibrary.as_view()),
+    path('spotify/devices', GetAvailableDevices.as_view()),
 ]
--- backend/api/utils.py
+++ backend/api/utils.py
@@ -4,12 +4,15 @@ from allauth.socialaccount.models import SocialToken
 # noinspection PyUnresolvedReferences
 from config import CLIENT_ID, CLIENT_SECRET
 from django.utils import timezone
-from requests import post
+from requests import post, put, get
+
+BASE_URL = "https://api.spotify.com/v1/"
+BASE_URL_ME = "https://api.spotify.com/v1/me/"
 
 
 def get_user_token(user):
     """:returns SocialToken object || None"""
-    user_tokens = SocialToken.objects.filter(user=user)
+    user_tokens = SocialToken.objects.filter(account__user=user)
     if user_tokens.exists():
         return user_tokens.first()
     return None
@@ -18,7 +21,7 @@ def get_user_token(user):
 def is_spotify_authenticated(user):
     token = get_user_token(user)
     if token:
-        expiry = token.expires_in
+        expiry = token.expires_at
         if expiry <= timezone.now():
             refresh_spotify_token(user)
         return True
@@ -49,3 +52,75 @@ def refresh_spotify_token(user):
     expires_in = response.get('expires_in')
 
     update_user_token(user, access_token, expires_in, refresh_token)
+
+
+def execute_spotify_api_call(user, endpoint, post_=False, put_=False, other_base_url=None):
+    spotify_token = get_user_token(user).token
+    headers = {'Content-Type': 'application/json', 'Authorization': "Bearer " + spotify_token}
+
+    url = BASE_URL_ME if not other_base_url else other_base_url
+
+    if post_:
+        response = post(url + endpoint, headers=headers)
+    elif put_:
+        response = put(url + endpoint, headers=headers)
+    else:
+        response = get(url + endpoint, {}, headers=headers)
+
+    # received empty object
+    if not response.text:
+        return response
+
+    try:
+        return response.json()
+    except Exception as e:
+        return {'Error': f"{e}"}
+
+
+def play_song(user):
+    return execute_spotify_api_call(user, "player/play", put_=True)
+
+
+def pause_song(user):
+    return execute_spotify_api_call(user, "player/pause", put_=True)
+
+
+def prev_song(user):
+    return execute_spotify_api_call(user, "player/previous", post_=True)
+
+
+def skip_song(user):
+    return execute_spotify_api_call(user, "player/next", post_=True)
+
+
+def set_volume(user, value):
+    return execute_spotify_api_call(user, f"player/volume?volume_percent={value}", put_=True)
+
+
+def search_for_items(user, query, types, limit=20):
+    return execute_spotify_api_call(
+        user,
+        endpoint=f"search?q={query}&type={types}&limit={limit}",
+        other_base_url=BASE_URL
+    )
+
+
+def add_to_queue(user, uri):
+    return execute_spotify_api_call(user, f"player/queue?uri={uri}", post_=True)
+
+
+def get_recommendations(user, track_id, limit=20):
+    return execute_spotify_api_call(user, f"recommendations?seed_tracks={track_id}&limit={limit}",
+                                    other_base_url=BASE_URL)
+
+
+def get_user_playlists(user, limit=20):
+    return execute_spotify_api_call(user, f"playlists?limit={limit}")
+
+
+def get_playlist(user, playlist_id):
+    return execute_spotify_api_call(user, endpoint=f"playlists/{playlist_id}", other_base_url=BASE_URL)
+
+
+def get_next_items(user, href):
+    return execute_spotify_api_call(user, endpoint="", other_base_url=href)
