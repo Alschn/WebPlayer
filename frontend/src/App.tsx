4192027579b3da0425914b280f955e68068b2663
Alschn <adam.lisichin@gmail.com>
2021-05-28 17:05:06 +0200

    Add UserContextProvider wrapper
    Added new component, which wraps provider, sends request to api, holds response data in state and passes it into provider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,16 @@ import React, {FC, Fragment} from 'react';
 import './App.scss';
 import {AuthContextProvider} from "./context/authContext";
 import Router from "./routes";
+import {UserContextProvider} from "./context/userContext";
 
 
 const App: FC = () => {
   return (
     <Fragment>
       <AuthContextProvider>
-        <Router/>
+        <UserContextProvider>
+          <Router/>
+        </UserContextProvider>
       </AuthContextProvider>
     </Fragment>
   );
--- frontend/src/context/userContext.tsx
+++ frontend/src/context/userContext.tsx
@@ -1,15 +1,55 @@
-import {createContext} from "react";
+import React, {createContext, FC, ReactNode, useEffect, useState} from "react";
+import axiosClient from "../utils/axiosClient";
+import useLocalStorage from "../hooks/useLocalStorage";
 
 interface UserContextProps {
   username: string | undefined;
   imageURL: string | undefined;
-  uid: string | undefined;
+  spotifyURL: string | undefined;
+  id: string | undefined;
+  followers: number | undefined;
+  hasPremium: boolean | undefined;
 }
 
 export const UserContext = createContext<UserContextProps>({
   username: undefined,
   imageURL: undefined,
-  uid: undefined,
+  spotifyURL: undefined,
+  id: undefined,
+  followers: undefined,
+  hasPremium: undefined,
 });
+
+interface UserContextProviderProps {
+  children: ReactNode;
+}
+
+export const UserContextProvider: FC<UserContextProviderProps> = ({children}) => {
+  const [token] = useLocalStorage('token');
+  const [userData, setUserData] = useState<UserContextProps>();
+
+  useEffect(() => {
+    if (!userData && token) {
+      axiosClient.get(`http://localhost:8000/api/spotify/users`)
+        .then(res => {
+          setUserData(res.data)
+        })
+        .catch(err => console.log(err))
+    }
+  }, [userData, token]);
+
+  return (
+    <UserContext.Provider value={{
+      username: userData?.username,
+      imageURL: userData?.imageURL,
+      spotifyURL: userData?.spotifyURL,
+      id: userData?.id,
+      followers: userData?.followers,
+      hasPremium: userData?.hasPremium,
+    }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
 
 export default UserContext;
