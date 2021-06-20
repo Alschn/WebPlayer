2cd6319a670df19b2d40445c3a5ffc536b96de99
Alschn <adam.lisichin@gmail.com>
2021-06-20 23:06:09 +0200

    Add mypy, django stubs, open api docs
    Installed mypy, django and drf stubs, drf-yasg - swagger and redoc for documentation, bumped all packages.

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -16,7 +16,22 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.generic import TemplateView
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="WebPlayer API",
+        default_version='v1',
+        description="API built with Django Rest Framework.",
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+# noinspection PyUnresolvedReferences
 urlpatterns = [
     path('admin/', admin.site.urls),
     # applications
@@ -25,6 +40,9 @@ urlpatterns = [
     path('rest-auth/', include('rest_auth.urls')),
     path('rest-auth/registration/', include('rest_auth.registration.urls')),
     path('accounts/', include('allauth.urls')),
+    # docs
+    path(r'api/swagger', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path(r'api/redoc', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     # react router
     re_path('.*', TemplateView.as_view(template_name='index.html')),
 ]
