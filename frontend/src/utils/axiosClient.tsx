8e52721e306d5c3d29099b11cd9ba5d8a14cc359
Alschn <adam.lisichin@gmail.com>
2021-08-27 13:46:31 +0200

    Replace previous implementation of Axios Client
    Added axios interceptors to make sure that token is checked before each request and attached to headers.

--- frontend/src/utils/axiosClient.tsx
+++ frontend/src/utils/axiosClient.tsx
@@ -1,22 +1,14 @@
 import axios from "axios";
 
-const client = (token: string | null = null) => {
-  const defaultOptions = {
-    headers: {
-      "Authorization": token ? `Token ${token}` : '',
-      "Content-Type": "application/json",
-    },
-  };
+const AxiosClient = axios.create({
+  headers: {'Content-Type': 'application/json'}
+});
 
-  return {
-    get: (url: string, options = {}) => axios.get(url, { ...defaultOptions, ...options }),
-    post: (url: string, data: object, options = {}) => axios.post(url, data, { ...defaultOptions, ...options }),
-    put: (url: string, data: object, options = {}) => axios.put(url, data, { ...defaultOptions, ...options }),
-    patch: (url: string, data: object, options = {}) => axios.patch(url, data, { ...defaultOptions, ...options }),
-    delete: (url: string, options = {}) => axios.delete(url, { ...defaultOptions, ...options }),
-  };
-};
-
-const AxiosClient = client(localStorage.getItem('token'));
+// before sending request attach auth token
+AxiosClient.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  config.headers.Authorization = token ? `Token ${token}` : '';
+  return config;
+});
 
 export default AxiosClient;
