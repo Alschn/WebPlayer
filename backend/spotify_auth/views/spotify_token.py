ac72bddc7a2490cdf08772548229dcc6c0f4eaaa
Alschn <67019937+Alschn@users.noreply.github.com>
2023-10-06 18:04:53 +0200

    Improve spotify_auth app code
    Improve docstrings, swagger docs, add extra serializers,

--- backend/spotify_auth/views/spotify_token.py
+++ backend/spotify_auth/views/spotify_token.py
@@ -1,6 +1,6 @@
 from typing import Any
 
-from rest_framework import status
+from rest_framework import status, serializers
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.request import Request
 from rest_framework.response import Response
@@ -10,10 +10,17 @@ from spotify_adapter.utils import get_user_token
 from spotify_auth.permissions import HasSpotifyToken
 
 
+class SpotifyTokenSerializer(serializers.Serializer):
+    token = serializers.CharField()
+
+
 class GetCurrentSpotifyTokenView(APIView):
-    """/api/auth/spotify/token"""
+    """
+    GET     /api/auth/spotify/token/    - Get the current user's Spotify token
+    """
 
     permission_classes = [IsAuthenticated, HasSpotifyToken]
+    serializer_class = SpotifyTokenSerializer
 
     def get(self, request: Request, *args: Any, **kwargs: Any) -> Response:
         token = get_user_token(request.user)
