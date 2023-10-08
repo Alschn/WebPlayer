08ba4f4d484ca7ca1ac970cfc021b0b83e1f2c6a
Alschn <67019937+Alschn@users.noreply.github.com>
2023-10-08 13:58:28 +0200

    Improve /spotify/me/ endpoint

--- backend/spotify_adapter/views/users/me.py
+++ backend/spotify_adapter/views/users/me.py
@@ -1,42 +1,46 @@
 from typing import Any
 
 from allauth.socialaccount.models import SocialAccount
-from rest_framework import status
+from rest_framework import status, serializers
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 
+class CurrentUserSerializer(serializers.Serializer):
+    display_name = serializers.CharField()
+    external_urls = serializers.DictField()
+    id = serializers.CharField()
+    images = serializers.ListField()
+    type = serializers.CharField()
+    uri = serializers.CharField()
+    followers = serializers.DictField()
+    country = serializers.CharField()
+    product = serializers.CharField()
+    explicit_content = serializers.DictField()
+    email = serializers.EmailField()
+
+
 class CurrentUserView(APIView):
     """
-    GET     /api/spotify/users/     - Get current user's data
+    GET     /api/spotify/me/
+
+    Get current user's data from the social account stored in the database.
     """
 
     permission_classes = [IsAuthenticated]
-
-    # todo: improve this view
+    serializer_class = CurrentUserSerializer
 
     def get(self, request: Request, *args: Any, **kwargs: Any) -> Response:
-        social = SocialAccount.objects.filter(user=request.user)
+        social_account = SocialAccount.objects.filter(
+            user=request.user,
+            provider='spotify',
+        )
 
-        if not social.exists():
+        if not social_account.exists():
             return Response(status=status.HTTP_404_NOT_FOUND)
 
-        social_acc = social.first()
-        user_data = social_acc.extra_data
-        image = user_data.get('images')
-        try:
-            image = image[0].get('url')
-        except Exception:
-            pass
-
-        followers = user_data.get('followers').get('total')
-
-        data = {
-            'username': user_data.get('display_name'),
-            'imageURL': image,
-            'id': user_data.get('id'),
-            'followers': followers,
-        }
-        return Response(data, status=status.HTTP_200_OK)
+        social_account = social_account.first()
+        json_data = social_account.extra_data
+        return Response(json_data, status=status.HTTP_200_OK)
