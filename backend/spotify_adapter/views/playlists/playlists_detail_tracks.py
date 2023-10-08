d5fe6f839ad81cc767f282c451c00b9104ad97d0
Alschn <67019937+Alschn@users.noreply.github.com>
2023-10-08 16:33:00 +0200

    Implement missing playlists tracks endpoints

--- backend/spotify_adapter/views/playlists/playlists_detail_tracks.py
+++ backend/spotify_adapter/views/playlists/playlists_detail_tracks.py
@@ -15,6 +15,48 @@ class PlaylistsDetailsTracksParamsSerializer(serializers.Serializer):
     additional_types = serializers.CharField(required=False)
 
 
+class PlaylistsDetailsTracksAddDataSerializer(serializers.Serializer):
+    position = serializers.IntegerField(
+        required=False, min_value=0,
+        help_text=(
+            "The position to insert the items, a zero-based index. "
+            "For example, to insert the items in the first position: position=0; "
+            "to insert the items in the third position: position=2. "
+            "If omitted, the items will be appended to the playlist. "
+            "Items are added in the order they are listed in the query string or request body."
+        )
+    )
+    uris = serializers.ListField(
+        child=serializers.CharField(),
+        required=True,
+        help_text=(
+            "A comma-separated list of Spotify URIs to add, can be track or episode URIs. "
+            "For example: uris=spotify:track:4iV5W9uYEdYUVa79Axb7Rh,spotify:track:1301WleyT98MSxVHPZCA6M "
+            "A maximum of 100 items can be added in one request."
+        )
+    )
+
+
+class PlaylistsDetailsTracksUpdateDataSerializer(serializers.Serializer):
+    uris = serializers.ListField(
+        child=serializers.CharField(),
+        required=False,
+    )
+    range_start = serializers.IntegerField(required=False)
+    insert_before = serializers.IntegerField(required=False)
+    range_length = serializers.IntegerField(required=False)
+    snapshot_id = serializers.CharField(required=False)
+
+
+class TracksSerializer(serializers.Serializer):
+    uri = serializers.CharField()
+
+
+class PlaylistsDetailsTracksRemoveDataSerializer(serializers.Serializer):
+    tracks = TracksSerializer(many=True)
+    snapshot_id = serializers.CharField(required=False)
+
+
 class PlaylistsDetailTracksView(APIView):
     """
     GET     /api/spotify/playlists/<str:playlist_id>/tracks/
@@ -60,22 +102,50 @@ class PlaylistsDetailTracksView(APIView):
         return Response(data, status=status.HTTP_200_OK)
 
     @extend_schema(
-
+        request=PlaylistsDetailsTracksAddDataSerializer,
+        # todo: response serializer
     )
     def post(self, request: Request, playlist_id: str, *args, **kwargs) -> Response:
-        # todo: add items to playlist
-        pass
+        serializer = PlaylistsDetailsTracksAddDataSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        client = get_spotify_client(request.user)
+        data = client.playlist_add_items(playlist_id, **serializer.validated_data)
+        return Response(data, status=status.HTTP_201_CREATED)
 
     @extend_schema(
-
+        request=PlaylistsDetailsTracksUpdateDataSerializer,
+        # todo: response serializer
     )
     def put(self, request: Request, playlist_id: str, *args, **kwargs) -> Response:
-        # todo: update playlist items
-        pass
+        serializer = PlaylistsDetailsTracksUpdateDataSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        uris = serializer.validated_data.pop('uris', None)
+
+        client = get_spotify_client(request.user)
+
+        if uris:
+            data = client.playlist_replace_items(
+                playlist_id, items=uris
+            )
+        else:
+            data = client.playlist_reorder_items(
+                playlist_id, **serializer.validated_data
+            )
+
+        return Response(data, status=status.HTTP_200_OK)
 
     @extend_schema(
-
+        request=PlaylistsDetailsTracksRemoveDataSerializer,
+        # todo: response serializer
     )
     def delete(self, request: Request, playlist_id: str, *args, **kwargs) -> Response:
-        # todo: remove items from playlist
-        pass
+        serializer = PlaylistsDetailsTracksRemoveDataSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        client = get_spotify_client(request.user)
+        data = client.playlist_remove_all_occurrences_of_items(
+            playlist_id, **serializer.validated_data
+        )
+        return Response(data, status=status.HTTP_200_OK)
