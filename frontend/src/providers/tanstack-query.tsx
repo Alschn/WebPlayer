f0aaf1ac4e6b8f6e74f091e2fa6f979ff7d3ae40
Alschn <67019937+Alschn@users.noreply.github.com>
2023-10-28 20:41:33 +0200

    Fix queryClient in query client provider
    Moved queryClient to provider's state

--- frontend/src/providers/tanstack-query.tsx
+++ frontend/src/providers/tanstack-query.tsx
@@ -1,15 +1,15 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import type { ReactNode } from "react";
-
-const queryClient = new QueryClient();
+import { useState, type ReactNode } from "react";
 
 export default function TanstackQueryClientProvider({
   children,
 }: {
   children: ReactNode;
 }) {
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
