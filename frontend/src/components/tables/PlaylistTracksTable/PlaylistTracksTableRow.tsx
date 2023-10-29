e29fb603465d758e82baf39861540bbeea4e0738
Alschn <67019937+Alschn@users.noreply.github.com>
2023-10-29 18:07:05 +0100

    Move playlist track row into separate component

---
+++ frontend/src/components/tables/PlaylistTracksTable/PlaylistTracksTableRow.tsx
@@ -0,0 +1,75 @@
+import Image from "next/image";
+import NextLink from "next/link";
+import type { PlaylistTrack } from "~/api/types";
+import { TableCell, TableRow } from "~/components/ui/table";
+import { getMsToTimeString, relativeTimeFromDates } from "~/lib/format";
+import DropdownMoreHorizMenu from "../SavedTracksTable/DropdownMoreHorizMenu";
+
+interface PlaylistTracksTableRowProps {
+  item: PlaylistTrack;
+  index: number;
+}
+
+const PlaylistTracksTableRow = ({
+  item,
+  index,
+}: PlaylistTracksTableRowProps) => {
+  return (
+    <TableRow key={`item` + item.track.id + index}>
+      <TableCell>{index + 1}</TableCell>
+      <TableCell>
+        <div className="align-center flex flex-row gap-2">
+          <Image
+            src={item.track.album.images?.[0]?.url ?? ""}
+            alt={item.track.album.name}
+            width={48}
+            height={48}
+          />
+          <div className="flex flex-col justify-center">
+            <span className="line-clamp-1 text-base dark:text-white">
+              {item.track.name}
+            </span>
+            <div>
+              {item.track.artists.map((artist, index, array) => (
+                <div
+                  key={`track-${item.track.id}-artist-${artist.id}`}
+                  className="inline-block"
+                >
+                  <NextLink
+                    href={`/artists/${artist.id}/`}
+                    className="hover:underline dark:text-gray-400 dark:hover:text-white"
+                  >
+                    {artist.name}
+                  </NextLink>
+                  {index !== array.length - 1 && (
+                    <span className="mr-1 dark:text-gray-400">{", "}</span>
+                  )}
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </TableCell>
+      <TableCell>
+        <NextLink
+          href={`/albums/${item.track.album.id}/`}
+          className="line-clamp-1 hover:underline dark:text-gray-400 dark:hover:text-white"
+        >
+          {item.track.album.name}
+        </NextLink>
+      </TableCell>
+      <TableCell className="dark:text-gray-400">
+        {relativeTimeFromDates(new Date(item.added_at))}
+      </TableCell>
+      <TableCell></TableCell>
+      <TableCell className="dark:text-gray-400">
+        {getMsToTimeString(item.track.duration_ms, true)}
+      </TableCell>
+      <TableCell>
+        <DropdownMoreHorizMenu />
+      </TableCell>
+    </TableRow>
+  );
+};
+
+export default PlaylistTracksTableRow;
--- frontend/src/components/tables/PlaylistTracksTable/index.tsx
+++ frontend/src/components/tables/PlaylistTracksTable/index.tsx
@@ -1,16 +1,12 @@
-import Image from "next/image";
-import NextLink from "next/link";
 import type { PlaylistTrack } from "~/api/types";
 import {
   Table,
   TableBody,
-  TableCell,
   TableHead,
   TableHeader,
   TableRow,
 } from "~/components/ui/table";
-import { getMsToTimeString, relativeTimeFromDates } from "~/lib/format";
-import DropdownMoreHorizMenu from "../SavedTracksTable/DropdownMoreHorizMenu";
+import PlaylistTracksTableRow from "./PlaylistTracksTableRow";
 
 interface PlaylistTracksTableProps {
   data: PlaylistTrack[];
@@ -32,62 +28,11 @@ const PlaylistTracksTable = ({ data }: PlaylistTracksTableProps) => {
       </TableHeader>
       <TableBody>
         {data.map((item, index) => (
-          <TableRow key={`item` + item.track.id + index}>
-            <TableCell>{index + 1}</TableCell>
-            <TableCell>
-              <div className="align-center flex flex-row gap-2">
-                <Image
-                  src={item.track.album.images?.[0]?.url ?? ""}
-                  alt={item.track.album.name}
-                  width={48}
-                  height={48}
-                />
-                <div className="flex flex-col justify-center">
-                  <span className="line-clamp-1 text-base dark:text-white">
-                    {item.track.name}
-                  </span>
-                  <div>
-                    {item.track.artists.map((artist, index, array) => (
-                      <div
-                        key={`track-${item.track.id}-artist-${artist.id}`}
-                        className="inline-block"
-                      >
-                        <NextLink
-                          href={`/artists/${artist.id}/`}
-                          className="hover:underline dark:text-gray-400 dark:hover:text-white"
-                        >
-                          {artist.name}
-                        </NextLink>
-                        {index !== array.length - 1 && (
-                          <span className="mr-1 dark:text-gray-400">
-                            {", "}
-                          </span>
-                        )}
-                      </div>
-                    ))}
-                  </div>
-                </div>
-              </div>
-            </TableCell>
-            <TableCell>
-              <NextLink
-                href={`/albums/${item.track.album.id}/`}
-                className="line-clamp-1 hover:underline dark:text-gray-400 dark:hover:text-white"
-              >
-                {item.track.album.name}
-              </NextLink>
-            </TableCell>
-            <TableCell className="dark:text-gray-400">
-              {relativeTimeFromDates(new Date(item.added_at))}
-            </TableCell>
-            <TableCell></TableCell>
-            <TableCell className="dark:text-gray-400">
-              {getMsToTimeString(item.track.duration_ms, true)}
-            </TableCell>
-            <TableCell>
-              <DropdownMoreHorizMenu />
-            </TableCell>
-          </TableRow>
+          <PlaylistTracksTableRow
+            key={`item-${item.track.id}-${index}`}
+            item={item}
+            index={index}
+          />
         ))}
       </TableBody>
     </Table>
