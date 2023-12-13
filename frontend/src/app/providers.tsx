bd085108fe09069b6b9cd9a8e0667a68fd18fefc
Alschn <67019937+Alschn@users.noreply.github.com>
2023-12-13 19:23:33 +0100

    Comment out SpotifyWebPlaybackSDKScript

--- frontend/src/app/providers.tsx
+++ frontend/src/app/providers.tsx
@@ -10,7 +10,7 @@ export default function Providers({ children }: { children: ReactNode }) {
   return (
     <TanstackQueryClientProvider>
       <AuthContextProvider>
-        <SpotifyWebPlaybackSDKScript />
+        {/* <SpotifyWebPlaybackSDKScript /> */}
         <WebPlaybackProvider>{children}</WebPlaybackProvider>
       </AuthContextProvider>
     </TanstackQueryClientProvider>
