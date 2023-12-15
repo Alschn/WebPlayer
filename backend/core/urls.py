96f0bd8f0c4afb587d52adba5f2aa6c95f750e28
Alschn <67019937+Alschn@users.noreply.github.com>
2023-12-15 22:40:12 +0100

    Add static urls to urlpatterns

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -28,8 +29,9 @@ urlpatterns = [
     path('api/', include('accounts.urls')),
     path('api/spotify/', include('spotify_adapter.urls')),
     path('api/auth/spotify/', include('spotify_auth.urls')),
-
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
     urlpatterns += [
