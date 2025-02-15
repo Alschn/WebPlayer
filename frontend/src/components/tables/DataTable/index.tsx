29cb91686bade688d910925e75d561a72881c0f9
Alschn <67019937+Alschn@users.noreply.github.com>
2025-02-15 21:38:37 +0100

    Install Tanstack Table, add DataTable component, rewrite PlaylistTracksTable
    Also added checkbox component from shadcn/ui (for debugging purposes)

---
+++ frontend/src/components/tables/DataTable/index.tsx
@@ -0,0 +1,124 @@
+"use client";
+
+import {
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  RowSelectionState,
+  useReactTable,
+  type Row,
+} from "@tanstack/react-table";
+import { useEffect, useRef, useState } from "react";
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "~/components/ui/table";
+import { useClickOutside } from "~/hooks/useClickOutside";
+import { useSelectRow } from "~/hooks/useSelectedRow";
+
+interface DataTableProps<TData, TValue> {
+  columns: ColumnDef<TData, TValue>[];
+  data: TData[];
+  onRowDoubleClick?: (row: Row<TData>) => void | Promise<void>;
+  resetRowSelectionOnClickOutside?: boolean;
+  selectRowOnClick?: boolean;
+}
+
+export default function DataTable<TData, TValue>({
+  columns,
+  data,
+  onRowDoubleClick,
+  resetRowSelectionOnClickOutside,
+  selectRowOnClick,
+}: DataTableProps<TData, TValue>) {
+  const [rowSelection, setRowSelection] = useState<RowSelectionState>({});
+
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    // row selection
+    enableMultiRowSelection: false,
+    onRowSelectionChange: setRowSelection,
+    // table state
+    state: {
+      rowSelection,
+    },
+  });
+
+  const { handleClick: handleRowClick } = useSelectRow<Row<TData>>({
+    onClick: (row) => {
+      if (selectRowOnClick) {
+        row.toggleSelected(true);
+      }
+    },
+    onDoubleClick: (row) => {
+      if (selectRowOnClick) {
+        row.toggleSelected(true);
+      }
+      onRowDoubleClick?.(row);
+    },
+  });
+
+  const tableRef = useRef<HTMLTableElement>(null);
+
+  useClickOutside(tableRef, () => {
+    if (!resetRowSelectionOnClickOutside) return;
+    table.resetRowSelection();
+  });
+
+  return (
+    <div className="rounded-md border">
+      <Table ref={tableRef}>
+        <TableHeader>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <TableRow key={headerGroup.id}>
+              {headerGroup.headers.map((header) => {
+                return (
+                  <TableHead key={header.id}>
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext(),
+                        )}
+                  </TableHead>
+                );
+              })}
+            </TableRow>
+          ))}
+        </TableHeader>
+        <TableBody>
+          {table.getRowModel().rows?.length ? (
+            table.getRowModel().rows.map((row) => (
+              <TableRow
+                key={row.id}
+                data-state={row.getIsSelected() ? "selected" : "idle"}
+                onClick={() => {
+                  handleRowClick(row);
+                }}
+              >
+                {row.getVisibleCells().map((cell) => (
+                  <TableCell key={cell.id}>
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))
+          ) : (
+            <TableRow>
+              <TableCell colSpan={columns.length} className="h-24 text-center">
+                No results.
+              </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    </div>
+  );
+}
--- frontend/src/components/tables/PlaylistTracksTable/index.tsx
+++ frontend/src/components/tables/PlaylistTracksTable/index.tsx
@@ -1,71 +1,117 @@
-"use client";
-
-import { useRef } from "react";
+import { ColumnDef, Row } from "@tanstack/react-table";
+import NextImage from "next/image";
+import NextLink from "next/link";
+import { useCallback } from "react";
 import { playSong } from "~/api/player";
-import type { PlaylistTrack } from "~/api/types";
-import {
-  Table,
-  TableBody,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "~/components/ui/table";
+import { PlaylistTrack } from "~/api/types";
+import DataTable from "~/components/tables/DataTable";
 import { useToast } from "~/components/ui/use-toast";
-import { useClickOutside } from "~/hooks/useClickOutside";
-import { useSelectRow } from "~/hooks/useSelectedRow";
-import PlaylistTracksTableRow from "./PlaylistTracksTableRow";
+import { getMsToTimeString, relativeTimeFromDates } from "~/lib/format";
+import DropdownMoreHorizMenu from "../SavedTracksTable/DropdownMoreHorizMenu";
 
 interface PlaylistTracksTableProps {
   data: PlaylistTrack[];
 }
 
+const columns: ColumnDef<PlaylistTrack>[] = [
+  {
+    header: "#",
+    cell: ({ row }) => <span>{row.index + 1}</span>,
+  },
+  {
+    header: "Title",
+    cell: ({ row }) => (
+      <div className="align-center flex flex-row gap-2">
+        <NextImage
+          src={row.original.track.album.images?.[0]?.url ?? ""}
+          alt={row.original.track.album.name}
+          width={48}
+          height={48}
+        />
+        <div className="flex flex-col justify-center">
+          <span className="line-clamp-1 text-base dark:text-white">
+            {row.original.track.name}
+          </span>
+          <div>
+            {row.original.track.artists.map((artist, index, array) => (
+              <div
+                key={`track-${row.original.track.id}-artist-${artist.id}`}
+                className="inline-block"
+              >
+                <NextLink
+                  href={`/artists/${artist.id}/`}
+                  className="hover:underline dark:text-stone-400 dark:hover:text-white"
+                >
+                  {artist.name}
+                </NextLink>
+                {index !== array.length - 1 && (
+                  <span className="mr-1 dark:text-stone-400">{", "}</span>
+                )}
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    ),
+  },
+  {
+    header: "Album",
+    cell: ({ row }) => (
+      <NextLink
+        href={`/albums/${row.original.track.album.id}/`}
+        className="line-clamp-1 hover:underline dark:text-stone-400 dark:hover:text-white"
+      >
+        {row.original.track.album.name}
+      </NextLink>
+    ),
+  },
+  {
+    header: "Date Added",
+    accessorKey: "added_at",
+    cell: (props) => (
+      <span className="dark:text-stone-400">
+        {relativeTimeFromDates(new Date(props.getValue<string>()))}
+      </span>
+    ),
+  },
+  {
+    header: "Time",
+    accessorKey: "track.duration_ms",
+    cell: (props) => (
+      <span className="dark:text-stone-400">
+        {getMsToTimeString(props.getValue<number>(), true)}
+      </span>
+    ),
+  },
+  {
+    id: "actions",
+    header: "",
+    cell: () => <DropdownMoreHorizMenu />,
+  },
+];
+
 const PlaylistTracksTable = ({ data }: PlaylistTracksTableProps) => {
   const { toast } = useToast();
-  const {
-    selected: selectedRow,
-    handleClick: handleSelectRow,
-    resetSelected,
-  } = useSelectRow<string>({
-    onDoubleClick(item) {
-      void playSong({ uri: item }).catch(() => {
-        toast({
-          title: "Could not play song!",
-          description: "Something went wrong...",
-          variant: "destructive",
-          duration: 2000,
-        });
-      });
-    },
-  });
 
-  const tableRef = useRef<HTMLTableElement>(null);
-  useClickOutside(tableRef, resetSelected);
+  const handleRowDoubleClick = useCallback(async (row: Row<PlaylistTrack>) => {
+    await playSong({ uri: row.original.track.uri }).catch(() => {
+      toast({
+        title: "Could not play song!",
+        description: "Something went wrong...",
+        variant: "destructive",
+        duration: 2000,
+      });
+    });
+  }, []);
 
   return (
-    <Table id="playlist-tracks-table" ref={tableRef}>
-      <TableHeader>
-        <TableRow>
-          <TableHead className="w-[40px]">#</TableHead>
-          <TableHead>Title</TableHead>
-          <TableHead>Album</TableHead>
-          <TableHead>Date Added</TableHead>
-          <TableHead></TableHead>
-          <TableHead>Time</TableHead>
-          <TableHead></TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {data.map((item, index) => (
-          <PlaylistTracksTableRow
-            key={`item-${item.track.id}-${index}`}
-            item={item}
-            index={index}
-            isSelected={selectedRow === item.track.uri}
-            onClick={() => handleSelectRow(item.track.uri)}
-          />
-        ))}
-      </TableBody>
-    </Table>
+    <DataTable
+      data={data}
+      columns={columns}
+      resetRowSelectionOnClickOutside
+      selectRowOnClick
+      onRowDoubleClick={handleRowDoubleClick}
+    />
   );
 };
 
